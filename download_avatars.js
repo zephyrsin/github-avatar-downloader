966401ae6a966a2e15907509feb24be9d8abf331
zephyrsin <1yl3@queensu.ca>
2017-03-27 22:48:22 -0400

    Able to download pictures

--- download_avatars.js
+++ download_avatars.js
@@ -1,4 +1,5 @@
 var request = require('request');
+var fs = require('fs');
 
 var GITHUB_USER = "zephyrsin";
 var GITHUB_TOKEN = "527676dda5bd7ff3fc8fe35eb21789b64bb40028";
@@ -7,15 +8,38 @@ var repoName = "jquery";
 
 console.log('Welcome to the GitHub Avatar Downloader!');
 
-var requestURL = 'https://'+ GITHUB_USER + ':' + GITHUB_TOKEN + '@api.github.com/repos/' + repoOwner + '/' + repoName + '/contributors';
+	var requestURL = {
+		url: 'https://'+ GITHUB_USER + ':' + GITHUB_TOKEN + '@api.github.com/repos/' + 
+repoOwner + '/' + repoName + '/contributors',
+		headers: {
+		      'User-Agent': 'Github Avator Downloader'
+		}
+	};
 
-console.log(requestURL);
+function getRepoContributors(repoOwner, repoName, cb) {
 
-//function getRepoContributors(repoOwner, repoName, cb) {
+	request(requestURL, function(err, response, body) {
+	if (err) throw err;
+  	console.log('Response Status Code:', response.statusCode);
+	var parse = JSON.parse(body);
+	for (attribute in parse) {
+		var filepath = "C:\\Users\\YangLu\\workspace\\github-avatar-downloader\\";
+		filepath += parse[attribute].login+".jpeg";
+		console.log(parse[attribute].avatar_url);
+		downloadImageByURL(parse[attribute].avatar_url,filepath);
+	}
+	});
+}
+
+function downloadImageByURL(url, filePath) {
   // ...
-//}
+  request(url, function(err,response,body) {
+  if (err) throw err;
+  }).pipe(fs.createWriteStream(filePath));
+  }
+	
+getRepoContributors("jquery", "jquery", function(err, result) {
+  console.log("Errors:", err);
+  console.log("Result:", result);
+});
 
-//getRepoContributors("jquery", "jquery", function(err, result) {
-//  console.log("Errors:", err);
- // console.log("Result:", result);
-//});
